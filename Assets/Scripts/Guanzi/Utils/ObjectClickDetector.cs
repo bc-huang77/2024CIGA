66bfe520bdf073099287857015541f83d7f8dec1
Urim_XXL <3051068921@qq.com>
2024-07-06 02:59:36 +0800

    glow not disappear when click UI

--- Assets/Scripts/Guanzi/Utils/ObjectClickDetector.cs
+++ Assets/Scripts/Guanzi/Utils/ObjectClickDetector.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ObjectClickDetector : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class ObjectClickDetector : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // 检测鼠标左键按下
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverUIObject()) // 检测鼠标左键按下
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
@@ -28,5 +29,14 @@ public class ObjectClickDetector : MonoBehaviour
                 spriteRenderer.material = originalMaterial; // 恢复原始材质
             }
         }
+    }
+    
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        System.Collections.Generic.List<RaycastResult> results = new System.Collections.Generic.List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
