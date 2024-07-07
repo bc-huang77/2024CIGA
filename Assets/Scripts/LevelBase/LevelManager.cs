64161404f1b15ea684c096067130461a946206e3
yang <Lyuxyang@gmail.com>
2024-07-07 09:21:57 +0800

    add Forest 关卡逻辑和角色死亡函数待修复。

--- Assets/Scripts/LevelBase/LevelManager.cs
+++ Assets/Scripts/LevelBase/LevelManager.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public bool isCaveUnlocked = true;
-    public bool isCaveClear = true;
-    public bool isOcenaUnlocked = true;
-    public bool isOcenaClear = true;
-    public bool isGlacierUnlocked = true;
-    public bool isGlacierClear = true;
+    public bool isForestUnlocked = false;
+    public bool isForestClear = false;
+    public bool isCaveUnlocked = false;
+    public bool isCaveClear = false;
+    public bool isOcenaUnlocked = false;
+    public bool isOcenaClear = false;
+    public bool isGlacierUnlocked = false;
+    public bool isGlacierClear = false;
     public static LevelManager Instance { get; private set; }
     private bool isFirstTimeLoadManga = true;
 
